1fe95baf4beab3b4872f8c38e6b2f92d12a696ae
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-13 16:41:58 +0530

    test

--- simple-authenticator-sigma-sample/signin-lambda.js
+++ simple-authenticator-sigma-sample/signin-lambda.js
@@ -1,10 +1,11 @@
 let AWS = require('aws-sdk');
+const s3 = new AWS.S3();
 let connectionManager = require('./ConnectionManager');
 let SL = require('@slappforge/slappforge-sdk');
 const rds = new SL.AWS.RDS(connectionManager);
 exports.handler = function (event, context, callback) {
 
-    let successfullyLoggedIn = false;
+	let successfullyLoggedIn = false;
 	let inserts = [event.email, event.password];
 	// Replace the query with the actual query
 	// You can pass the existing connection to this function.
@@ -23,5 +24,21 @@ exports.handler = function (event, context, callback) {
 
 		connection.end();
 		callback(null, successfullyLoggedIn);
+		
 	});
+
+	s3.getBucketLocation({
+			'Bucket': "hiru.demo01"
+		}).promise()
+			.then(data => {
+				console.log(data);           // successful response
+				/*
+				data = {
+					LocationConstraint: "us-west-2"
+				}
+				*/
+			})
+			.catch(err => {
+				console.log(err, err.stack); // an error occurred
+			});
 }
